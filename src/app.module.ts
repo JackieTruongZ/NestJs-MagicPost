72f4d25fffc922c6b784ffa2f20ce843650e7f39
JackieTruongZ <datnguyentruongnn@gmail.com>
2023-12-27 22:52:48 +0700

    fix all

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { RoleModule } from "./Role/role.module";
@@ -8,7 +9,6 @@ import { ProductModule } from "./product/product.module";
 import { TransModule } from "./TransactionManager/trans.module";
 import { HubModule } from "./HubManager/hub.module";
 import { OrderModule } from "./Order/order.module";
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
